c3823a81b8b8a5c395afd6c2d4e71c075244f3cb
santimontironi <santiimontironi@gmail.com>
2025-05-10 04:22:52 -0300

    modelo Producto creado

--- productosapp/models.py
+++ productosapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=200)
+    stock = models.PositiveIntegerField(default=0)
+    precio = models.DecimalField(max_digits=8,decimal_places=2)
+    imagen = models.ImageField(upload_to='productos/', blank=True, null=True)
