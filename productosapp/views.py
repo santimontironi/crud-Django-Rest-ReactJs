4d6a66c8b554882d7d4308799392f0c84a0a39f0
santimontironi <santiimontironi@gmail.com>
2025-05-10 04:29:38 -0300

    viewSet creado

--- productosapp/views.py
+++ productosapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Producto
+from .serializer import ProductoSerializer
 
-# Create your views here.
+# Create your views here...
+class ProductoView(viewsets.ModelViewSet):
+    serializer_class = ProductoSerializer
+    queryset = Producto.objects.all()
