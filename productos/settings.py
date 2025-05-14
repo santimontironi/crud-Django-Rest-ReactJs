40a7cb639fa78aea764b1c6861cba6611773a47c
santimontironi <santiimontironi@gmail.com>
2025-05-14 15:56:31 -0300

    variables de entorno creadas para mas seguridad

--- productos/settings.py
+++ productos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0u@l2^8+np3a0&5qkq25)p_x!1oa5i#)lc0)$o(tc_i92=c^v1'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,9 +80,9 @@ WSGI_APPLICATION = 'productos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'gestor',        
-        'USER': 'django',                 
-        'PASSWORD': 'django',
+        'NAME': config('DATABASE_NAME'),        
+        'USER': config('DATABASE_USER'),                 
+        'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': 'localhost',              
         'PORT': '5432'                
     }
