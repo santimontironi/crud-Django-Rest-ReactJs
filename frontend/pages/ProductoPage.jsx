6d0e67a0dfe9bf0d51cfa7d82eb655e26b355069
santimontironi <santiimontironi@gmail.com>
2025-05-19 19:49:47 -0300

    formulario busqueda agregado en el page ProductoPage

--- frontend/pages/ProductoPage.jsx
+++ frontend/pages/ProductoPage.jsx
@@ -15,15 +15,25 @@ export const ProductoPage = () => {
   },[])
 
   return (
-    <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
+    <div>
 
-      {productos.map(producto => (
-          <Producto key={producto.id} id={producto.id} nombre={producto.nombre} precio={producto.precio} stock={producto.stock} descripcion={producto.descripcion}/>
-      ))}
+      <form className="mt-4 flex gap-5 align-center justify-center m-auto" method="post">
+          <input className="bg-white text-dark shadow-[10px_5px_10px_#000] p-2 w-[300px]" type="text" placeholder="Busqueda de producto" />
+          <input className="bg-black text-white p-1 w-[70px] cursor-pointer hover:bg-cyan-950" type="submit" value="Buscar" />
+      </form>
 
-      {productos.length == 0 && (
-        <h1>No hay productos agregados.</h1>
-      )}
+      <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
+
+        {productos.map(producto => (
+            <Producto key={producto.id} id={producto.id} nombre={producto.nombre} precio={producto.precio} stock={producto.stock} descripcion={producto.descripcion}/>
+        ))}
+
+        {productos.length == 0 && (
+          <h1>No hay productos agregados.</h1>
+        )}
+      </div>
+
     </div>
+    
   )
 }
