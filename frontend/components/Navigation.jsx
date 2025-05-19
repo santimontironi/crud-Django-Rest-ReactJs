445d2d5f7fe47ee85d06b368a1b85d62b1d93c24
santimontironi <santiimontironi@gmail.com>
2025-05-19 19:32:32 -0300

    formulario busqueda agregado

--- frontend/components/Navigation.jsx
+++ frontend/components/Navigation.jsx
@@ -2,13 +2,18 @@ import { Link } from "react-router-dom";
 
 export const Navigation = () => {
   return (
-    <div className="flex justify-between px-8 pt-5 border-b-4 border-b-white">
+    <div className="flex justify-between px-8 p-5 border-b-4 border-b-white">
         <Link to='/productos'>
-            <h2 className="text-white text-[30px]">Productos</h2>
+            <h2 className="text-white text-[30px] hover:bg-amber-300 hover:text-black p-1">Productos</h2>
         </Link>
 
+        <form className="py-4 flex gap-3" method="post">
+          <input type="text" placeholder="Busqueda de producto" />
+          <input type="submit" value="Buscar" />
+        </form>
+
         <Link to='/productoForm'>
-            <h2 className="text-white text-[30px]">Formulario</h2>
+            <h2 className="text-white text-[30px] hover:bg-amber-300 hover:text-black p-1">Formulario</h2>
         </Link>
     </div>
   )
