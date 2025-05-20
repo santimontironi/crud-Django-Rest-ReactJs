fb5622260d78ca9ac255182acd3206fbe023f0a4
santimontironi <santiimontironi@gmail.com>
2025-05-20 19:01:45 -0300

    ahora si funciona la busqueda correctamente

--- frontend/pages/ProductoPage.jsx
+++ frontend/pages/ProductoPage.jsx
@@ -6,6 +6,7 @@ export const ProductoPage = () => {
 
   const[productos,setProductos] = useState([])
   const[busqueda,setBusqueda] = useState('')
+  const[modoBusqueda,setModoBusqueda] = useState(false)
 
   useEffect(() => {
     async function loadProductos() {
@@ -15,6 +16,11 @@ export const ProductoPage = () => {
     loadProductos()
   },[])
 
+  function handleForm(e){
+    e.preventDefault()
+    setModoBusqueda(true)
+  }
+
   const productosFiltrados = productos.filter(producto =>
     producto.nombre.toLowerCase().includes(busqueda.toLowerCase())
   )
@@ -22,39 +28,41 @@ export const ProductoPage = () => {
   return (
     <div className="pb-5">
 
-      <form className="mt-4 flex gap-5 align-center justify-center m-auto" method="post" onSubmit={(e) => e.preventDefault()}>
+      <form className="mt-4 flex gap-5 align-center justify-center m-auto" method="post" onSubmit={handleForm}>
           <input className="bg-white text-dark shadow-[10px_5px_10px_#000] p-2 w-[300px]" type="text" placeholder="Busqueda de producto" onChange={(e) => setBusqueda(e.target.value)} />
           <input className="bg-black text-white p-1 w-[70px] cursor-pointer hover:bg-cyan-950" type="submit" value="Buscar" />
       </form>
 
-      <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
+      {modoBusqueda ? (
+        <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
 
-        {productos.map(producto => (
-            <Producto key={producto.id} id={producto.id} nombre={producto.nombre} precio={producto.precio} stock={producto.stock} descripcion={producto.descripcion}/>
-        ))}
+          {productos.map(producto => (
+              <Producto key={producto.id} id={producto.id} nombre={producto.nombre} precio={producto.precio} stock={producto.stock} descripcion={producto.descripcion}/>
+          ))}
 
-        {productos.length == 0 && (
-          <h1>No hay productos agregados.</h1>
-        )}
-      </div>
-
-      <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
-        {productosFiltrados.length > 0 ? (
-          productosFiltrados.map((producto) => (
-            <Producto
-              key={producto.id}
-              id={producto.id}
-              nombre={producto.nombre}
-              precio={producto.precio}
-              stock={producto.stock}
-              descripcion={producto.descripcion}
-            />
-          ))
-        ) : (
-          <h1>No hay productos agregados.</h1>
-        )}
-      </div>
-
+          {productos.length == 0 && (
+            <h1>No hay productos agregados.</h1>
+          )}
+        </div>
+      ) : (
+        <div className="md:grid md:grid-cols-3 w-full mt-7 place-content-center place-items-center gap-3 sm:flex sm:flex-col sm:justify-center sm:items-center">
+          {productosFiltrados.length > 0 ? (
+            productosFiltrados.map((producto) => (
+              <Producto
+                key={producto.id}
+                id={producto.id}
+                nombre={producto.nombre}
+                precio={producto.precio}
+                stock={producto.stock}
+                descripcion={producto.descripcion}
+              />
+            ))
+          ) : (
+            <h1>No hay productos agregados.</h1>
+          )}
+        </div>
+      )}
+    
     </div>
     
   )
