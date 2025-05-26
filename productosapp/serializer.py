84cd0878e83fe4f84aa26301a3785fdf9ea5b485
santimontironi <santiimontironi@gmail.com>
2025-05-25 21:56:36 -0300

    renderizado de las fotos correctamente

--- productosapp/serializer.py
+++ productosapp/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Producto
 
 class ProductoSerializer(serializers.ModelSerializer):
-    imagen = serializers.ImageField(use_url=True)
+    imagen = serializers.ImageField(use_url=False)
     class Meta:
         model = Producto
         fields = '__all__'
