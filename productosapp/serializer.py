d4a3d844a7a2ab2389c49277bbf729a9529d69a9
santimontironi <santiimontironi@gmail.com>
2025-05-26 23:54:04 -0300

    configuracion del User en el serializer.py

--- productosapp/serializer.py
+++ productosapp/serializer.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Producto
+from django.contrib.auth.models import User
 
 class ProductoSerializer(serializers.ModelSerializer):
     imagen = serializers.ImageField(use_url=True)
     class Meta:
         model = Producto
         fields = '__all__'
+        
+    class Meta:
+        model = User
+        fields = ['username','password','email']
+        
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
