a53bd0053c409769f03a1b72b54178f9d02ce77e
santimontironi <santiimontironi@gmail.com>
2025-05-27 11:38:13 -0300

    logica del componente Auth.jsx agregada

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { login } from "../api/fetch"
+
 export const Auth = () => {
+  const[error,setError] = useState("")
+  const[username,setUsername] = useState("")
+  const[password,setPassword] = useState("")
+  const navigate = useNavigate()
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    try{
+      await login(username,password)
+      navigate('/productos')
+    }
+    catch(error){
+      setError("Usuario o contraseña incorrectos")
+      console.log(error)
+    }
+  }
+
   return (
-    <div>Auth</div>
+    <div className="login">
+      <h2>Iniciar sesión</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+        <input type="password" placeholder="Clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        <input type="submit" value="Ingresar" />
+      </form>
+      {error &&(
+        <p>Hubo un error: {error}</p>
+      )}
+    </div>
   )
 }
