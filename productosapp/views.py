16a9af4ed3141a4ffb428c38738dcdab852de764
santimontironi <santiimontironi@gmail.com>
2025-05-26 23:59:33 -0300

    permiso para estar logueado agregar en views.py

--- productosapp/views.py
+++ productosapp/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .models import Producto
 from .serializer import ProductoSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here...
 class ProductoView(viewsets.ModelViewSet):
     serializer_class = ProductoSerializer
     queryset = Producto.objects.all()
+    permission_classes = [IsAuthenticated]
+
