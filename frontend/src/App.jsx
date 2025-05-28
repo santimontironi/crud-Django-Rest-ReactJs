aa87e7f8152f6f5db75a3a959f5f72b5b72744df
santimontironi <santiimontironi@gmail.com>
2025-05-28 10:57:50 -0300

    estructura mejora en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,33 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
-import { ProductoPage } from "../pages/ProductoPage"
-import { FormPage } from "../pages/FormPage"
-import { Auth } from "../pages/Auth"
-import { Navigation } from "../components/Navigation"
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { ProductoPage } from "../pages/ProductoPage";
+import { FormPage } from "../pages/FormPage";
+import { Auth } from "../pages/Auth";
+import { Navigation } from "../components/Navigation";
+
+
+const LayoutWithNav = () => (
+  <>
+    <Navigation />
+    <Outlet />
+  </>
+);
 
 export const App = () => {
   return (
     <div className="min-h-screen w-full bg-blue-600">
       <BrowserRouter>
         <Routes>
+        
           <Route path="/" element={<Navigate to="/auth" />} />
+          
           <Route path="/auth" element={<Auth />} />
-          <Route path="*" element={
-            <>
-              <Navigation />
-              <Routes>
-                <Route path="/productos" element={<ProductoPage />} />
-                <Route path="/productoForm" element={<FormPage />} />
-                <Route path="/producto/:id" element={<FormPage />} />
-              </Routes>
-            </>
-          } />
+
+          <Route element={<LayoutWithNav />}>
+            <Route path="/productos" element={<ProductoPage />} />
+            <Route path="/productoForm" element={<FormPage />} />
+            <Route path="/producto/:id" element={<FormPage />} />
+          </Route>
+          
         </Routes>
       </BrowserRouter>
     </div>
