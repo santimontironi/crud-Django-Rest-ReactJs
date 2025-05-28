1a2163874d82d2d85845dd0b2039b01979aad3bb
santimontironi <santiimontironi@gmail.com>
2025-05-28 10:50:51 -0300

    mejora en App.jsx para que el componente Auth no tenga el componente Navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { ProductoPage } from "../pages/ProductoPage";
-import { FormPage } from "../pages/FormPage";
-import { Auth } from "../pages/Auth";
-import { Navigation } from "../components/Navigation";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import { ProductoPage } from "../pages/ProductoPage"
+import { FormPage } from "../pages/FormPage"
+import { Auth } from "../pages/Auth"
+import { Navigation } from "../components/Navigation"
 
 export const App = () => {
   return (
     <div className="min-h-screen w-full bg-blue-600">
       <BrowserRouter>
-        <Navigation/>
         <Routes>
-          <Route path="/" element={<Navigate to = '/auth' />} />
-          <Route path="/auth" element={<Auth/>}/>
-          <Route path="/productos" element={<ProductoPage/>} />
-          <Route path="/productoForm" element={<FormPage/>} />
-          <Route path="/producto/:id" element={<FormPage/>} />
+          <Route path="/" element={<Navigate to="/auth" />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="*" element={
+            <>
+              <Navigation />
+              <Routes>
+                <Route path="/productos" element={<ProductoPage />} />
+                <Route path="/productoForm" element={<FormPage />} />
+                <Route path="/producto/:id" element={<FormPage />} />
+              </Routes>
+            </>
+          } />
         </Routes>
       </BrowserRouter>
     </div>
-  )
-}
-
+  );
+};
