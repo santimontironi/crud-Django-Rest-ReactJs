73b2c5a2fcd60b23948aed37dc4fe52fcc9c8f18
santimontironi <santiimontironi@gmail.com>
2025-05-29 17:30:57 -0300

    estilos para componente Auth

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -23,11 +23,17 @@ export const Auth = () => {
   return (
     <div className="w-full min-h-screen">
       <h2 className="w-full h-[250px] text-[80px] text-center flex justify-center items-center tracking-wider bg-gradient-to-r from-yellow-100 shadow-[5px_5px_16px_#000] to-yellow-200">Iniciar sesión</h2>
-      <form className="text-white" onSubmit={handleSubmit}>
-        <input className="text-white" type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
-        <input className="text-white" type="password" placeholder="Clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-        <input type="submit" value="Ingresar" />
-        <p>Si no tenés una cuenta:s <Link className="bg-red-600 p-3 rounded-2xl hover:bg-red-800" to="/register">Regístrate</Link></p>
+      <form className="text-white flex flex-col justify-center w-[450px] h-[300px] m-auto items-center mt-10 border-3 border-yellow-200 shadow-[7px_8px_15px_#000] hover:bg-blue-700" onSubmit={handleSubmit}>
+        <div className="mb-3 flex flex-col gap-2 items-start">
+          <label htmlFor="usuario">Usuario:</label>
+          <input className="text-white p-3 w-[260px] border-2 border-yellow-200" type="text" id="usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+        </div>
+        <div className="mb-3 flex flex-col gap-2 items-start">
+          <label htmlFor="clave">Clave:</label>
+          <input className="text-white p-3 w-[260px] border-2 border-yellow-200" type="password" id="clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+        </div>
+        <input className="p-3 bg-amber-300 text-black rounded-2xl" type="submit" value="Ingresar" />
+        <p className="mt-3">Si no tenés una cuenta:  <Link className="underline text-yellow-200 hover:text-yellow-300" to="/register">Regístrate</Link></p>
       </form>
       {error &&(
         <p>Hubo un error: {error}</p>
