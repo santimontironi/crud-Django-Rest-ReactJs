3ea84d09e2c5c9422b65911fde6426d4eb8088c9
santimontironi <santiimontironi@gmail.com>
2025-05-29 17:15:20 -0300

    estilos en el titulo de Auth

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import { login } from "../api/fetch"
 
 export const Auth = () => {
@@ -21,12 +21,13 @@ export const Auth = () => {
   }
 
   return (
-    <div className="login">
-      <h2>Iniciar sesión</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
-        <input type="password" placeholder="Clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+    <div className="w-full min-h-screen">
+      <h2 className="w-full h-[250px] text-[80px] text-center flex justify-center items-center tracking-wider bg-gradient-to-r from-yellow-100 shadow-[5px_5px_16px_#000] to-yellow-200">Iniciar sesión</h2>
+      <form className="text-white" onSubmit={handleSubmit}>
+        <input className="text-white" type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+        <input className="text-white" type="password" placeholder="Clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
         <input type="submit" value="Ingresar" />
+        <p>Si no tenés una cuenta:s <Link className="bg-red-600 p-3 rounded-2xl hover:bg-red-800" to="/register">Regístrate</Link></p>
       </form>
       {error &&(
         <p>Hubo un error: {error}</p>
