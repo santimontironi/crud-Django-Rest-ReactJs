6d634faeec92f1429f47d1a3be448b3febc33424
santimontironi <santiimontironi@gmail.com>
2025-05-30 16:44:32 -0300

    mejoras en ux ui de Auth.jsx

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -3,9 +3,9 @@ import { useNavigate, Link } from "react-router-dom"
 import { login } from "../api/fetch"
 
 export const Auth = () => {
-  const[error,setError] = useState("")
   const[username,setUsername] = useState("")
   const[password,setPassword] = useState("")
+  const[errorAuth,setErrorAuth] = useState("")
   const navigate = useNavigate()
 
   async function handleSubmit(e){
@@ -15,15 +15,16 @@ export const Auth = () => {
       navigate('/productos')
     }
     catch(error){
-      setError("Usuario o contraseña incorrectos")
-      console.log(error)
+      setErrorAuth(error)
+      setUsername("")
+      setPassword("")
     }
   }
 
   return (
     <div className="w-full min-h-screen">
       <h2 className="w-full h-[250px] text-[80px] text-center flex justify-center items-center tracking-wider bg-gradient-to-r from-yellow-100 shadow-[5px_5px_16px_#000] to-yellow-200">Iniciar sesión</h2>
-      <form className="text-white flex flex-col justify-center w-[450px] h-[300px] m-auto items-center mt-10 border-3 border-yellow-200 shadow-[7px_8px_15px_#000] hover:bg-blue-700" onSubmit={handleSubmit}>
+      <form className="text-white flex flex-col justify-center w-[450px] h-[auto] p-4 m-auto items-center mt-6 border-3 border-yellow-200 shadow-[7px_8px_15px_#000] hover:bg-blue-700" onSubmit={handleSubmit}>
         <div className="mb-3 flex flex-col gap-2 items-start">
           <label htmlFor="usuario">Usuario:</label>
           <input className="text-white p-3 w-[260px] border-2 border-yellow-200" type="text" id="usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
@@ -35,8 +36,8 @@ export const Auth = () => {
         <input className="p-3 bg-amber-300 text-black rounded-2xl cursor-pointer hover:bg-amber-500" type="submit" value="Ingresar" />
         <p className="mt-3">Si no tenés una cuenta:  <Link className="underline text-yellow-200 hover:text-yellow-300" to="/register">Regístrate</Link></p>
       </form>
-      {error &&(
-        <p>Hubo un error: {error}</p>
+      {errorAuth &&(
+        <p>Datos no válidos</p>
       )}
     </div>
   )
