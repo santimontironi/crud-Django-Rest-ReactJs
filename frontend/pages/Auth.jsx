183d6b3a1d3c2e1616609ce6e1ecf96647a24626
santimontironi <santiimontironi@gmail.com>
2025-05-30 19:03:44 -0300

    fondos nuevos para Auth y Register

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
 import { login } from "../api/fetch"
+import fondoAuth from '../src/assets/img/fondoAuth.jpg'
 
 export const Auth = () => {
   const[username,setUsername] = useState("")
@@ -22,7 +23,7 @@ export const Auth = () => {
   }
 
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen bg-no-repeat bg-cover bg-center bg-gradient-to-b from-black/70 to-black/90 bg-blend-overlay"style={{ backgroundImage: `url(${fondoAuth})` }}>
       <h2 className="w-full h-[250px] text-[80px] text-center flex justify-center items-center tracking-wider bg-gradient-to-r from-yellow-100 shadow-[5px_5px_16px_#000] to-yellow-200">Iniciar sesión</h2>
       <form className="text-white flex flex-col justify-center w-[450px] h-[auto] p-4 m-auto items-center mt-6 border-3 border-yellow-200 shadow-[7px_8px_15px_#000] hover:bg-blue-700" onSubmit={handleSubmit}>
         <div className="mb-3 flex flex-col gap-2 items-start">
@@ -34,7 +35,7 @@ export const Auth = () => {
           <input className="text-white p-3 w-[260px] border-2 border-yellow-200" type="password" id="clave" value={password} onChange={(e) => setPassword(e.target.value)} required/>
         </div>
         <input className="p-3 bg-amber-300 text-black rounded-2xl cursor-pointer hover:bg-amber-500" type="submit" value="Ingresar" />
-        <p className="mt-3">Si no tenés una cuenta:  <Link className="underline text-yellow-200 hover:text-yellow-300" to="/register">Regístrate</Link></p>
+        <p className="mt-3">Si no tenés una cuenta:  <Link className="underline text-yellow-500" to="/register">Regístrate</Link></p>
       </form>
       {errorAuth &&(
         <p>Datos no válidos</p>
--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom";
 import { registerUser } from "../api/fetch"
+import fondoRegistro from '../src/assets/img/fondoRegistro.jpg'
 
 export const Register = () => {
 
@@ -26,7 +27,7 @@ export const Register = () => {
   }
 
   return (
-    <div className="w-full min-h-screen">
+    <div className="w-full min-h-screen bg-no-repeat bg-cover bg-center bg-gradient-to-b from-black/70 to-black/90 bg-blend-overlay"style={{ backgroundImage: `url(${fondoRegistro})` }}>
         <h2 className="w-full h-[250px] text-[80px] text-center text-white flex justify-center items-center tracking-wider bg-gradient-to-r from-blue-500 to-blue-700 shadow-[5px_5px_16px_#000]">Registro</h2>
         <form className="text-white flex flex-col justify-center w-[450px] h-[auto] p-4 m-auto items-center mt-5 border-3 border-yellow-200 shadow-[7px_8px_15px_#000] hover:bg-blue-700" onSubmit={handleRegister}>
           <div className="mb-3 flex flex-col gap-2 items-start">
