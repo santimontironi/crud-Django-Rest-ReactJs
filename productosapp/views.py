fe840cc5733757ff91ced6f72964024864ea0104
santimontironi <santiimontironi@gmail.com>
2025-06-02 13:05:18 -0300

    funcion send_mail implementada

--- productosapp/views.py
+++ productosapp/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from .serializer import ProductoSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated
+from productos.settings import EMAIL_USERNAME as emisor
 
 # Create your views here...
 class ProductoView(viewsets.ModelViewSet):
@@ -17,3 +18,14 @@ class UserView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = [AllowAny]  # Permite acceso sin token de autenticación
+    
+    #esta función es para manejar logica una vez creado el usuario
+    def perform_create(self, serializer):
+        user = serializer.save()
+        send_mail(
+            subject='Bienvenido al sistema.',
+            message=f'Hola {user.username}, gracias por registrarte en tuGestor.',
+            from_email=emisor,
+            recipient_list=[user.email],
+            fail_silently=False,  #Si hay un error, no se ignora
+        )
