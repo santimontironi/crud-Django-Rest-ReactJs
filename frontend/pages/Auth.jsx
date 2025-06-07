e9a6fd74f3faf1eba835bf3027552183205b1de9
santimontironi <santiimontironi@gmail.com>
2025-06-07 17:24:54 -0300

    Auth responsive

--- frontend/pages/Auth.jsx
+++ frontend/pages/Auth.jsx
@@ -38,8 +38,8 @@ export const Auth = () => {
 
   return (
     <div className="w-full min-h-screen bg-no-repeat bg-cover bg-center bg-gradient-to-b from-black/70 to-black/90 bg-blend-overlay"style={{ backgroundImage: `url(${fondoAuth})` }}>
-      <h2 className="w-full h-[180px] text-[80px] text-center flex justify-center items-center text-white">Iniciar sesión</h2>
-      <form className="text-white flex flex-col justify-center w-[450px] h-[auto] p-4 m-auto items-center mt-7 border-3 border-yellow-200 hover:shadow-[7px_8px_15px_#000]" onSubmit={handleSubmit}>
+      <h2 className="w-full md:h-[180px] lg:h-[200px] h-[200px] md:text-[80px] lg:text-[95px] text-[60px] text-center flex justify-center items-center text-white">Iniciar sesión</h2>
+      <form className="text-white flex flex-col justify-center md:w-[450px] lg:w-[450px] w-[350px] h-[auto] p-4 m-auto items-center mt-7 border-3 border-yellow-200 hover:shadow-[7px_8px_15px_#000]" onSubmit={handleSubmit}>
         <div className="mb-3 flex flex-col gap-2 items-start">
           <label htmlFor="usuario">Usuario:</label>
           <input className="text-white p-3 w-[260px] border-2 border-yellow-200" type="text" id="usuario" value={username} onChange={(e) => setUsername(e.target.value)} required/>
