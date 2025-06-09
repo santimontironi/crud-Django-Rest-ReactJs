9b7319a77616e2889be0d9245dba1c66e415821b
santimontironi <santiimontironi@gmail.com>
2025-06-09 14:56:10 -0300

    nuevas credenciales de la base de datos en produccion

--- productos/settings.py
+++ productos/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
         'NAME': config('DATABASE_NAME'),        
         'USER': config('DATABASE_USER'),                 
         'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': 'localhost',              
-        'PORT': '5432'                
+        'HOST': config('DATABASE_HOST'),              
+        'PORT': config('DATABASE_PORT')                
     }
 }
 
